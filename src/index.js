a90eac8adc086ea25d2bd451dc10e0afa8cb6dea
Udeozor-Ijay <92553461+Udeozor-Ijay@users.noreply.github.com>
2022-06-19 20:11:44 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// complete the function
+function prime (num) {
+
+  if (num===1)
+  {
+    return false;
+  }
+  else if(num === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < num; x++)
+    {
+      if(num % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+
+  // code goes here
+
+
+function solution (arg) {
+  return prime(arg);
+}
+
+if (typeof require !== "undefined" && require.main === module) {
+  if (process.argv.length <= 2) {
+    throw Error("Require argument");
+  }
+
+  const arg = process.argv[2];
+  console.log(solution(arg));
+}
+
+module.exports = { solution };
